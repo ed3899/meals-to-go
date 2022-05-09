30b25c0c4be7d4bd280d7177a97daea778ba4bac
Edward Casanova <cb.inv7613@gmail.com>
2022-05-09 15:03:02 -0500

    solution app layout

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
+//% libs
 import {StatusBar} from "expo-status-bar";
+
+import {SafeAreaView} from "react-native-safe-area-context";
+
 import {StyleSheet, Text, View} from "react-native";
+
+import {Fragment} from "react";
 
 const styles = StyleSheet.create({
   container: {
@@ -12,8 +18,16 @@ const styles = StyleSheet.create({
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Our react native canvas</Text>
-    </View>
+    <Fragment>
+      <SafeAreaView style={{flex: 1}}>
+        <View style={{padding: 16, backgroundColor: "green"}}>
+          <Text>Search</Text>
+        </View>
+        <View style={{flex: 1, padding: 16, backgroundColor: "blue"}}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <StatusBar style="auto" />
+    </Fragment>
   );
 }
