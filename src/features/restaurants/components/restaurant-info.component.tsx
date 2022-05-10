e88e4bf5939743c31956b0e1f41400aac6084732
Edward Casanova <cb.inv7613@gmail.com>
2022-05-10 10:15:19 -0500

    restaurant info setup

--- src/features/restaurants/components/restaurant-info.component.tsx
+++ src/features/restaurants/components/restaurant-info.component.tsx
@@ -2,6 +2,35 @@ import React from "react";
 
 import { Text } from "react-native";
 
-export default function RestaurantInfo() {
-   return <Text>Restaurant Info</Text>;
-}
+type RestaurantInfoPropsT = {
+   restaurant_?: Partial<{
+      name_: string;
+      icon_: unknown;
+      photos_: string[];
+      address_: string;
+      rating_: number;
+      isOpenNow_: boolean;
+      isClosedTemporarily_: boolean;
+   }>;
+};
+
+const RestaurantInfo: React.FC<RestaurantInfoPropsT> = ({
+   restaurant_ = {}
+}) => {
+   //! Safeguard undefined
+   const {
+      name_ = "Some restaurant",
+      icon_,
+      photos_ = [
+         "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg"
+      ],
+      address_ = "100 some random street",
+      isOpenNow_ = true,
+      rating_ = 4,
+      isClosedTemporarily_
+   } = restaurant_;
+
+   return <Text>{name_}</Text>;
+};
+
+export default RestaurantInfo;
