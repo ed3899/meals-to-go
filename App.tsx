7eba1d10ae90fe798216c0d5b7c012ef9def78f7
Edward Casanova <cb.inv7613@gmail.com>
2022-05-10 09:37:58 -0500

    added search bar

--- App.tsx
+++ App.tsx
@@ -1,21 +1,19 @@
 //% libs
-
 import React, { Fragment } from "react";
 
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Searchbar } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const styles = StyleSheet.create({
    container: {
       flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center"
+      backgroundColor: "#fff"
    },
    search: {
       padding: 16,
-      backgroundColor: "green"
+      backgroundColor: "white"
    },
    list: {
       flex: 1,
@@ -29,7 +27,7 @@ export default function App() {
       <Fragment>
          <SafeAreaView style={styles.container}>
             <View style={styles.search}>
-               <Text>Search</Text>
+               <Searchbar value="" />
             </View>
             <View style={styles.list}>
                <Text>List</Text>
