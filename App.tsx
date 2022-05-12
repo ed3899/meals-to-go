23b28cc261b964b06f5c6b3458fb38191f23e271
Edward Casanova <cb.inv7613@gmail.com>
2022-05-12 08:02:09 -0500

    before React Native update! npx expo install react-native@0.68.2

--- App.tsx
+++ App.tsx
@@ -9,8 +9,11 @@ import {
    createBottomTabNavigator,
    BottomTabScreenProps
 } from "@react-navigation/bottom-tabs";
-import { NavigationContainer } from "@react-navigation/native";
-import { NavigatorScreenParams } from "@react-navigation/native";
+import {
+   NavigationContainer,
+   NavigatorScreenParams,
+   CompositeScreenProps
+} from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { Button, Image, Text, View } from "react-native";
@@ -21,15 +24,15 @@ import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import RestaurantsScreen from "./src/features/restaurants/screens/restaurants.screen";
 import { ThemeProvider, theme } from "./src/infrastructure/theme";
 
-type RootStackParamList = {
-   Home: undefined;
-   Profile: undefined;
-   Settings: { userId: string } | undefined;
+type TabsParamList = {
+   Feed: { feedParam: string } | undefined;
+   Messages: undefined;
 };
 
-type TabsParamList = {
-   Feed: undefined;
-   Messages: undefined;
+type RootStackParamList = {
+   Home: NavigatorScreenParams<TabsParamList>;
+   Profile: undefined;
+   Settings: { userId: string } | undefined;
 };
 
 type DProps = NativeStackScreenProps<RootStackParamList, "Settings", "MyStack">;
@@ -37,10 +40,15 @@ type HomeProps = NativeStackScreenProps<RootStackParamList, "Home", "MyStack">;
 const RootStack = createNativeStackNavigator<RootStackParamList>();
 
 type FeedProps = BottomTabScreenProps<TabsParamList, "Feed", "MyTab">;
+
+type FeedScreenNavigationProps = CompositeScreenProps<
+   BottomTabScreenProps<TabsParamList, "Feed">,
+   NativeStackScreenProps<RootStackParamList>
+>;
 type MessagesProps = BottomTabScreenProps<TabsParamList, "Messages", "MyTab">;
 const Tab = createBottomTabNavigator<TabsParamList>();
 
-function Feed({ navigation }: FeedProps) {
+function Feed({ navigation }: FeedScreenNavigationProps) {
    return (
       <>
          <Text>Feed!</Text>
@@ -71,6 +79,17 @@ function Home({ navigation }: HomeProps) {
             <Tab.Screen name="Feed" component={Feed} />
             <Tab.Screen name="Messages" component={Messages} />
          </Tab.Navigator>
+         <Button
+            title="Button"
+            onPress={() =>
+               navigation.navigate("Home", {
+                  screen: "Feed",
+                  params: {
+                     feedParam: "d"
+                  }
+               })
+            }
+         ></Button>
       </>
    );
 }
