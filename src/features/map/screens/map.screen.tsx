ccf931471f6f6fe6aac985edd006eb7fa5ee179e
Edward Casanova <cb.inv7613@gmail.com>
2022-05-14 17:11:30 -0500

    174.5 - map screen and search

--- src/features/map/screens/map.screen.tsx
+++ src/features/map/screens/map.screen.tsx
@@ -2,6 +2,19 @@ import React from "react";
 
 import MapView from "react-native-maps";
 
-const MapScreen = () => <MapView style={{ height: "100%" }} />;
+import styled from "../../../infrastructure/theme";
+
+const Map = styled(MapView)`
+   height: "100%";
+   width: "100%";
+`;
+
+const MapScreen = () => {
+   return (
+      <>
+         <MapView style={{ height: "100%", width: "100%" }} />
+      </>
+   );
+};
 
 export default MapScreen;
