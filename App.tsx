d9b1ef84b41b0ab1e3d83b528f6fdd1afab2b54f
Edward Casanova <cb.inv7613@gmail.com>
2022-05-16 17:02:01 -0500

    new firebase config - ref https://stackoverflow.com/questions/72179070/react-native-bundling-failure-error-message-while-trying-to-resolve-module-i

--- App.tsx
+++ App.tsx
@@ -5,9 +5,9 @@ import {
    useFonts as useOswald,
    Oswald_400Regular
 } from "@expo-google-fonts/oswald";
-// eslint-disable-next-line import/no-named-as-default
-import Constants from "expo-constants";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+
+import { initializeApp } from "firebase/app";
 
 import Navigation from "./src/infrastructure/navigation";
 import { ThemeProvider, theme } from "./src/infrastructure/theme";
@@ -15,15 +15,21 @@ import { FavouritesContextProvider } from "./src/services/favourites/favourites.
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { RestaurantContextProvider } from "./src/services/restaurants/restaurant.context";
 
-// const firebaseConfig: firebaseApps.FirebaseOptions = {
-//    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-//    apiKey: Constants.manifest?.extra!.FIREBASE_API_KEY,
-//    authDomain: "mealstogo-1b561.firebaseapp.com",
-//    projectId: "mealstogo-1b561",
-//    storageBucket: "mealstogo-1b561.appspot.com",
-//    messagingSenderId: "726985536994",
-//    appId: "1:726985536994:web:aae2d52a4cec08470db5bb"
-// };
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+   apiKey: "",
+   authDomain: "mealstogo-1b561.firebaseapp.com",
+   projectId: "mealstogo-1b561",
+   storageBucket: "mealstogo-1b561.appspot.com",
+   messagingSenderId: "726985536994",
+   appId: "1:726985536994:web:aae2d52a4cec08470db5bb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const App = () => {
    const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -37,7 +43,6 @@ const App = () => {
    });
 
    if (!oswaldLoaded || !latoLoaded) return <Fragment></Fragment>;
-   if (!isAuthenticated) return <Fragment></Fragment>;
 
    return (
       <Fragment>
