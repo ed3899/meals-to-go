291b677df808a8424010848796b926f68007126f
Edward Casanova <cb.inv7613@gmail.com>
2022-05-18 13:47:08 -0500

    218 - Solution Expo Camera

--- src/features/settings/screens/camera.screen.tsx
+++ src/features/settings/screens/camera.screen.tsx
@@ -1,5 +1,37 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 
-const CameraScreen = () => null;
+import { Camera, CameraType } from "expo-camera";
+import { View } from "react-native";
+
+import { CustomText } from "../../../components/typography/text.component";
+import styled from "../../../infrastructure/theme";
+
+const ProfileCamera = styled(Camera)`
+   width: 100%;
+   height: 100%;
+`;
+
+const CameraScreen = () => {
+   const [hasPermission, setHasPermission] = useState(false);
+   const cameraRef = useRef<Camera | null>();
+
+   useEffect(() => {
+      (async () => {
+         const { granted } = await Camera.requestCameraPermissionsAsync();
+         setHasPermission(granted);
+      })();
+   }, []);
+
+   if (hasPermission === false) {
+      return <CustomText>No access to camera</CustomText>;
+   }
+
+   return (
+      <ProfileCamera
+         ref={camera => (cameraRef.current = camera)}
+         type={CameraType.front}
+      ></ProfileCamera>
+   );
+};
 
 export default CameraScreen;
