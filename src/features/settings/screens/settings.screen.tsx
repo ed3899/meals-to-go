a3cb19baf0f10c13e6a89a09fad89ede6cb7e8a1
Edward Casanova <cb.inv7613@gmail.com>
2022-05-18 09:43:56 -0500

    208 - making the settings screen look nice

--- src/features/settings/screens/settings.screen.tsx
+++ src/features/settings/screens/settings.screen.tsx
@@ -1,25 +1,47 @@
 /* eslint-disable react/prop-types */
 import React, { useContext } from "react";
 
-import { List } from "react-native-paper";
+import { List, Avatar } from "react-native-paper";
 
-import type {
-   Settings_ScreenT,
-   Settings_Stack_Screen_PropsT
-} from "../../../../@types";
+import styled from "../../../infrastructure/theme";
+
+import type { Settings_ScreenT } from "../../../../@types";
+
+import { CustomText } from "../../../components/typography/text.component";
+import Spacer from "../../../components/spacer/spacer.component";
 
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import AuthenticationContext from "../../../services/authentication/authentication.context";
 
+const SettingsItem = styled(List.Item)`
+   padding: ${props => props.theme.space[3]};
+`;
+
+const AvatarContainer = styled.View`
+   align-items: center;
+`;
+
 const SettingsScreen: Settings_ScreenT = ({ navigation }) => {
-   const { onLogout } = useContext(AuthenticationContext);
+   const { onLogout, user } = useContext(AuthenticationContext);
 
    return (
       <SafeArea>
+         <AvatarContainer>
+            <Avatar.Icon
+               size={180}
+               icon="human"
+               style={{ backgroundColor: "#2182BD" }}
+            />
+
+            <Spacer position="top" size="large" />
+
+            <CustomText variant="label">
+               {(user && user.email) ?? "No user"}
+            </CustomText>
+         </AvatarContainer>
+
          <List.Section>
-            <List.Item
-               // eslint-disable-next-line react-native/no-inline-styles
-               style={{ padding: 16 }}
+            <SettingsItem
                title="Favourites"
                description="View your favourites"
                left={props => (
@@ -28,9 +50,7 @@ const SettingsScreen: Settings_ScreenT = ({ navigation }) => {
                onPress={() => navigation.navigate("Favourites")}
             />
 
-            <List.Item
-               // eslint-disable-next-line react-native/no-inline-styles
-               style={{ padding: 16 }}
+            <SettingsItem
                title="Logout"
                left={props => (
                   <List.Icon {...props} color="black" icon="door" />
