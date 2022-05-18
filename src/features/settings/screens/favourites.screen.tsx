330a100e7012763cb0b869c7e98d42526e9a09bc
Edward Casanova <cb.inv7613@gmail.com>
2022-05-18 09:47:47 -0500

    209 - favourites screen exercise

---
+++ src/features/settings/screens/favourites.screen.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const FavouritesScreen = () => null;
+
+export default FavouritesScreen;
--- src/infrastructure/navigation/settings.navigator.tsx
+++ src/infrastructure/navigation/settings.navigator.tsx
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import type { Settings_Stack_ParamListT } from "../../../@types";
 
+import FavouritesScreen from "../../features/settings/screens/favourites.screen";
 import SettingsScreen from "../../features/settings/screens/settings.screen";
 
 const SettingsStack = createNativeStackNavigator<Settings_Stack_ParamListT>();
@@ -21,7 +22,7 @@ const SettingsNavigator = () => {
 
          <SettingsStack.Screen
             name="Favourites"
-            component={() => null}
+            component={FavouritesScreen}
          ></SettingsStack.Screen>
       </SettingsStack.Navigator>
    );
